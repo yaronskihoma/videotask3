8ade9bd4bb60e7e34e401c645d7a023f8e0cd647
yaronskihoma <123949299+yaronskihoma@users.noreply.github.com>
2024-11-17 07:51:28 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+const config = {
+    GOOGLE_SCRIPT_URL: 'YOUR_GOOGLE_SCRIPT_URL', // We'll create this URL in step 2
+    IPINFO_TOKEN: '6ef0f26335447e',
+    MORPHCAST_LICENSE: 'sk9bebf5467e79a53a382ee56f1293c7e03b406b94f0af'
+};
+
+export default config;
