c08ea684505ec414933d22c8bb683f5c7c186b6d
yaronskihoma <123949299+yaronskihoma@users.noreply.github.com>
2024-11-17 07:59:21 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-const config = {
-    GOOGLE_SCRIPT_URL: 'YOUR_GOOGLE_SCRIPT_URL', // We'll create this URL in step 2
-    IPINFO_TOKEN: '6ef0f26335447e',
-    MORPHCAST_LICENSE: 'sk9bebf5467e79a53a382ee56f1293c7e03b406b94f0af'
-};
-
-export default config;
