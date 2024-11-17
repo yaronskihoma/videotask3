d502b10a692b0bad1b8f94a6f00f6f925f46c0b4
yaronskihoma <123949299+yaronskihoma@users.noreply.github.com>
2024-11-17 09:12:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     SCRIPT_ID: 'arosualdata', // From script's URL
-    DEPLOYMENT_ID: 'AKfycbzkJuMIyWcZ7DAg9bhHY770K4YhMAWKHASBtXl9kn8MpF3mu38DAb8d_8axcfuGSB8g', // From deployment settings
+    DEPLOYMENT_ID: 'AKfycbwZKFTHKfh7woMWCwm1N1-YECtXj27civ-r4gX9faXD3-yjQJQmoqZjcoF8trbsCbFu', // From deployment settings
     BASE_URL: 'https://script.google.com/macros/s/',
     GOOGLE_SCRIPT_DEPLOYMENT_URL: 'AKfycbwZKFTHKfh7woMWCwm1N1-YECtXj27civ-r4gX9faXD3-yjQJQmoqZjcoF8trbsCbFu/exec',
     IPINFO_TOKEN: '6ef0f26335447e',
