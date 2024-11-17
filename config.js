bf7a7b4f3a18abef6b8e967f11537a0615d35a8a
yaronskihoma <123949299+yaronskihoma@users.noreply.github.com>
2024-11-17 09:30:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const config = {
-    SCRIPT_ID: 'arosualdata', // From script's URL
-    DEPLOYMENT_ID: 'AKfycbz3wmcPgkDgqCy8Q6-6bUTPDK9c5EEzlTSDhULFylboFfLbREzUCJO3rTkV_TQUC0Ce', // From deployment settings
+    SCRIPT_ID: 'arosualdata',
+    DEPLOYMENT_ID: 'AKfycbz3wmcPgkDgqCy8Q6-6bUTPDK9c5EEzlTSDhULFylboFfLbREzUCJO3rTkV_TQUC0Ce',
     BASE_URL: 'https://script.google.com/macros/s/',
-    GOOGLE_SCRIPT_DEPLOYMENT_URL: 'AKfycbz3wmcPgkDgqCy8Q6-6bUTPDK9c5EEzlTSDhULFylboFfLbREzUCJO3rTkV_TQUC0Ce/exec',
+    // Fix the deployment URL by combining BASE_URL + DEPLOYMENT_ID + '/exec'
+    GOOGLE_SCRIPT_DEPLOYMENT_URL: 'https://script.google.com/macros/s/AKfycbz3wmcPgkDgqCy8Q6-6bUTPDK9c5EEzlTSDhULFylboFfLbREzUCJO3rTkV_TQUC0Ce/exec',
     IPINFO_TOKEN: '6ef0f26335447e',
     MORPHCAST_LICENSE: 'sk9bebf5467e79a53a382ee56f1293c7e03b406b94f0af'
 };
