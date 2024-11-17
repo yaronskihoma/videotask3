c1e0489b565e3b8e1317840c303a310314c8f7fc
yaronskihoma <123949299+yaronskihoma@users.noreply.github.com>
2024-11-17 08:51:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     SCRIPT_ID: 'arosualdata', // From script's URL
     DEPLOYMENT_ID: 'AKfycbzkJuMIyWcZ7DAg9bhHY770K4YhMAWKHASBtXl9kn8MpF3mu38DAb8d_8axcfuGSB8g', // From deployment settings
     BASE_URL: 'https://script.google.com/macros/s/',
-    GOOGLE_SCRIPT_DEPLOYMENT_URL: 'https://script.google.com/macros/s/AKfycbzkJuMIyWcZ7DAg9bhHY770K4YhMAWKHASBtXl9kn8MpF3mu38DAb8d_8axcfuGSB8g/exec',
+    GOOGLE_SCRIPT_DEPLOYMENT_URL: 'AKfycbwZKFTHKfh7woMWCwm1N1-YECtXj27civ-r4gX9faXD3-yjQJQmoqZjcoF8trbsCbFu/exec',
     IPINFO_TOKEN: '6ef0f26335447e',
     MORPHCAST_LICENSE: 'sk9bebf5467e79a53a382ee56f1293c7e03b406b94f0af'
 };
