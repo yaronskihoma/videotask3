a6de69c6480e93dd599d64c4a9c9ce550e23ad70
yaronskihoma <123949299+yaronskihoma@users.noreply.github.com>
2024-11-17 17:47:50 +0100

    Update and rename config.example.js to config.js

--- config.example.js
+++
@@ -1,9 +0,0 @@
-// config.example.js
-const config = {
-    SUPABASE_URL: 'your-supabase-url',
-    SUPABASE_KEY: 'your-supabase-key',
-    MORPHCAST_LICENSE: 'your-morphcast-license',
-    IPINFO_TOKEN: 'your-ipinfo-token'
-};
-
-export default config;
